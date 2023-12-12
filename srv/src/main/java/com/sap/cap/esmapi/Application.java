e340d1e1ee84f8ce8c4f2fd495ef9ac4afceab2c
Sunny Bhardwaj <sunny13nitk@gmail.com>
2023-12-12 17:02:10 +0530

    auto conf enable - main

--- srv/src/main/java/com/sap/cap/esmapi/Application.java
+++ srv/src/main/java/com/sap/cap/esmapi/Application.java
@@ -1,12 +1,13 @@
 package com.sap.cap.esmapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-
+@EnableAutoConfiguration
 public class Application
 {
 
