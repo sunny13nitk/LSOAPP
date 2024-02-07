a80d98b2776f009a9ca82741a310ec43d3ee99b3
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-02-07 12:30:29 +0530

    logout redirect

--- srv/src/main/java/com/sap/cap/esmapi/ui/controllers/LogoutController.java
+++ srv/src/main/java/com/sap/cap/esmapi/ui/controllers/LogoutController.java
@@ -4,19 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Controller
 @RequestMapping("/logout")
-@Slf4j
 public class LogoutController
 {
 
     @GetMapping("/")
     public String showLogout()
     {
-        log.info("Logout Trigerred....");
-        return "logout";
+        String redirectUrl = "https://learninghub.sap.com/logout";
+        return "redirect:" + redirectUrl;
     }
 
 }
