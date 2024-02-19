0dd7c5cd34943bbef841dd2e96a647d1a6c62ceb
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-02-19 18:50:52 +0530

    Initial commit

--- srv/src/main/java/com/sap/cap/esmapi/Application.java
+++ srv/src/main/java/com/sap/cap/esmapi/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class Application
+public class Application 
 {
 
 	public static void main(String[] args)
