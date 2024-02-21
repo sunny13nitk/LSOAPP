048016b2f17131bfba49aa11d28c1086dc76149e
Sunny Bhardwaj <I057386>
2024-02-21 07:50:12 +0000

    maven version update

--- srv/src/gen/java/cds/gen/db/esmlogs/Esmlogs_.java
+++ srv/src/gen/java/cds/gen/db/esmlogs/Esmlogs_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "cds-maven-plugin",
-    date = "2024-02-20T16:55:33.281307100Z",
+    date = "2024-02-21T07:46:00.221011665Z",
     comments = "com.sap.cds:cds-maven-plugin:1.30.2 / com.sap.cds:cds4j-api:1.34.2"
 )
 @CdsName("db.esmlogs")
